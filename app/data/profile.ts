49eb961d997cd5c9d4b8260fea6a250415599298
Axel <axelmychro@gmail.com>
2026-09-10 22:41:17 +0700

    init data

---
+++ app/data/profile.ts
@@ -0,0 +1,30 @@
+import profile_picture from "@/public/profile.webp";
+
+export const profile = {
+  name: "ARATO Studio",
+  bio: "Edit Video, Buat Website.",
+  picture: profile_picture,
+
+  links: [
+    {
+      label: "Instagram",
+      icon: "mdi:instagram",
+      to: "https://www.instagram.com/axelmychro",
+    },
+    {
+      label: "WhatsApp",
+      icon: "mdi:instagram",
+      to: "https://wa.me/6285261230455",
+    },
+    {
+      label: "GitHub",
+      icon: "simple-icons:github",
+      to: "https://github.com/arato-studio",
+    },
+    {
+      label: "Email",
+      icon: "lucide:mail",
+      to: "mailto:axelmychro@gmail.com",
+    },
+  ],
+};
