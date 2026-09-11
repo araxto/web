90ec07f24bf408b81e734666d640efd5b947cde8
Axel <axelmychro@gmail.com>
2026-09-11 13:48:58 +0700

    add cta links

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,18 +20,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="id" className={`${go_mono.variable} h-full antialiased`}>
-      <body className="min-h-full min-w-full flex flex-col">
-        <header className="bg-background-2 items-center border-background-3 border-b-4 p-2 flex min-h-20">
+    <html lang="id" className={`${go_mono.variable} antialiased`}>
+      <body className="min-h-screen flex flex-col overflow-hidden">
+        <header className="bg-background-2 min-w-full items-center px-2 border-background-3 border-b-4 py-2 flex min-h-16 shrink-0">
           <Link href="#home">
-            <h1 className="text-3xl p-2 tracking-tighter ">{profile.name}</h1>
+            <h1 className="tracking-tighter">{profile.name}</h1>
           </Link>
         </header>
 
-        <main className="flex-1">{children}</main>
+        <main className="flex-1 flex flex-col items-center  px-8 py-8 overflow-auto gap-8">
+          {children}
+        </main>
 
-        <footer className="bg-background-2 items-center border-background-3 border-t-4 p-2 flex min-h-20">
-          <small className="text-[1rem]">
+        <footer className="bg-background-2 min-w-full items-center px-2 border-background-3 border-t-4 py-2 flex min-h-16 shrink-0">
+          <small>
             &copy;{" "}
             <time dateTime={new Date().toISOString()}>
               {new Date().getFullYear()}
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,44 @@
+import Link from "next/link";
+import { profile } from "./data/profile";
+import { Icon } from "@iconify/react";
+
 export default function Home() {
   return (
     <>
-      <section></section>
-      <section></section>
+      <section className="flex flex-col items-center justify-center gap-8 max-w-prose">
+        <h2 className="text-xl text-center text-balance">
+          Bikin{" "}
+          <b>
+            <i>Brand</i> Kamu
+          </b>{" "}
+          Tampil Beda Lewat Website & Video <b>Profesional</b>.
+        </h2>
+
+        <p className="max-w-md text-balance border-l-muted border-l-4 pl-4">
+          Kami bantu UMKM dan kreator ngembangin kehadiran digital lewat desain
+          web modern dan editing video yang memikat.
+        </p>
+      </section>
+
+      <section>
+        <h2></h2>
+        <ul className="flex gap-2 flex-wrap">
+          {profile.links.map((link) => (
+            <li
+              key={link.label}
+              className="bg-primary font-bold border-4 border-primary p-2 rounded-lg"
+            >
+              <Link
+                href={link.to}
+                className="flex items-center gap-4 min-w-32 text-text-rpd"
+              >
+                <Icon icon={link.icon} />
+                {link.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </section>
     </>
   );
 }
